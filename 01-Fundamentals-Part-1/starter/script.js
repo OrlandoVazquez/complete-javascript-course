117f97ee0b8739269f3192eeb3e7862ef51276cb
Orlando Nahuel Vázquez <ovazquez.gonzalez@gmail.com>
2025-03-05 18:14:22 -0400

    JavaScript Fundamentals - Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,22 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN!");
-40 + 8 + 23 - 10;
+console.log("Jonas!");
+console.log(23);
 console.log(40 + 8 + 23 - 10);
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let orlando_y_raquel = "OR";
+let $function = 27;
+let person = "Nahuel";
+let PI = 3.1415;
+
+let myFirstJob = "Coder"; //it's better to name variables with descriptive names
+let myCurrentJob = "Programmer";
+
+let job1 = "Coder";
+let job2 = "Programmer";
+
+console.log(myFirstJob);

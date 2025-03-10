082d795d9cb7e5fb6146ceaf5ce964b2e23a9762
Orlando Nahuel Vázquez <ovazquez.gonzalez@gmail.com>
2025-03-09 22:34:38 -0400

    JavaScript Fundamentals - Values and Variables Assignment

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+let country = "Bolivia";
+let continent = "América";
+let population = "12.564.085";
+console.log(
+  "Mi país actual es " +
+    country +
+    " está en el continente de " +
+    continent +
+    " y tiene una población de " +
+    population +
+    " habitantes."
+);

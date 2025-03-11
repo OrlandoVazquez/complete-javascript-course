6e49b1b57968093e9a190d2aabfd19cf401cd62a
Orlando Nahuel Vázquez <ovazquez.gonzalez@gmail.com>
2025-03-11 18:43:13 -0400

    JavaScript Fundamentals - Basic Operators

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -43,4 +43,52 @@ let language = "Spanish";
 const isIsland = false;
 
 //isIsland = True;
+
+Basic Operators
+If your country split in half, and each half would contain half the population, then how many people would live in each half?
+
+Increase the population of your country by 1 and log the result to the console.
+
+Finland has a population of 6 million. Does your country have more people than Finland?
+
+The average population of a country is 33 million people. Does you country have less people than the average country?
+
+Based on the variables you created, create a new variable description which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'.
 */
+
+const country = "Bolivia";
+const continent = "América";
+let language = "Spanish";
+const isIsland = false;
+let population = 12564085;
+const halfPopulation = population / 2;
+console.log(
+  "Si dividieramos " +
+    country +
+    " por la mitad, vivirían " +
+    halfPopulation +
+    " personas en cada parte."
+);
+
+population++;
+console.log(population);
+
+let finlandPopulation = 6000000;
+const isFinlandMorePopulated = finlandPopulation > population;
+console.log(
+  "Tiene " +
+    country +
+    " más población que Finlandia? : " +
+    isFinlandMorePopulated
+);
+const averageCountryPopulation = 30000000;
+const isConutryPopulationBellowAverage = population < averageCountryPopulation;
+console.log(
+  country +
+    " is in " +
+    continent +
+    ", and its " +
+    population / 1000000 +
+    " million people speak " +
+    language
+);
